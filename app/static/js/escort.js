e811c2ed507530eea32ef2eedd19d545d6a98b3c
Seongha Kim <hosh2326@naver.com>
2023-12-26 10:48:51 +0900

    add escort + modify index.css

--- app/static/js/escort.js
+++ app/static/js/escort.js
@@ -1,39 +1,55 @@
 var radioContainer = document.getElementById("radioContainer");
 
-for (var i = 1; i <= 22; i++) {
-    var radio = document.createElement("input");
-    radio.type = "radio";
-    radio.id = "radio" + i;
-    radio.name = "number";
-    radio.value = i;
+// Create radio buttons from 1 to 6
+for (var i = 1; i <= 6; i++) {
+  var radio = createRadioButton(i);
+  radioContainer.appendChild(radio);
+}
 
-    var label = document.createElement("label");
-    label.htmlFor = "radio" + i;
-    label.innerText = i;
+// Create radio buttons from 17 to 22
+for (var j = 17; j <= 22; j++) {
+  var radio = createRadioButton(j);
+  radioContainer.appendChild(radio);
+}
 
-    radioContainer.appendChild(radio);
-    radioContainer.appendChild(label);
+function createRadioButton(value) {
+  var radio = document.createElement("input");
+  radio.type = "radio";
+  radio.id = "radio" + value;
+  radio.name = "number";
+  radio.value = value;
+
+  var label = document.createElement("label");
+  label.htmlFor = "radio" + value;
+  label.innerText = value;
+
+  var wrapper = document.createElement("div");
+  wrapper.appendChild(radio);
+  wrapper.appendChild(label);
+
+  return wrapper;
 }
 
 function startGuide() {
-    var selectedNumber = document.querySelector('input[name="number"]:checked');
-    if (selectedNumber) {
-        alert(selectedNumber.value + '번 출입구로 안내를 시작합니다.');
-        // 여기에 안내 시작에 대한 로직 추가
-    } else {
-        alert('라디오 버튼을 선택하세요.');
-    }
+  var selectedNumber = document.querySelector('input[name="number"]:checked');
+  if (selectedNumber) {
+    alert(selectedNumber.value + "번 출입구로 안내를 시작합니다.");
+    // 여기에 안내 시작에 대한 로직 추가
+  } else {
+    alert("라디오 버튼을 선택하세요.");
+  }
 }
 
+// Rest of your map-related code remains unchanged
 var mapOptions = {
-    center: new naver.maps.LatLng(35.14863950517846, 126.91899292171), // 출입구 위치 좌표로 변경
-    zoom: 17, // 지도 확대 레벨 조정
+  center: new naver.maps.LatLng(35.1476504, 126.9189039), // 출입구 위치 좌표로 변경
+  zoom: 17, // 지도 확대 레벨 조정
 };
 
-var map = new naver.maps.Map('map', mapOptions);
+var map = new naver.maps.Map("map", mapOptions);
 
 // 출입구 위치에 마커 추가
 var marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(35.14863950517846, 126.91899292171), // 출입구 위치 좌표로 변경
-    map: map,
+  position: new naver.maps.LatLng(35.1476504, 126.9189039), // 출입구 위치 좌표로 변경
+  map: map,
 });
